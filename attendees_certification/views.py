dc7a81b9d4ffe755640a21c7feed994b3cfbdd31
Annanda Sousa <annanda.sousa@gmail.com>
2015-12-06 18:59:28 -0200

    Adicionado login required na view upload

--- attendees_certification/views.py
+++ attendees_certification/views.py
@@ -2,13 +2,16 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
 
+from braces.views import LoginRequiredMixin
+
 from .forms import AttendeesForm
 
 
-class AttendeesUploadFormView(FormView):
+class AttendeesUploadFormView(LoginRequiredMixin, FormView):
     form_class = AttendeesForm
     template_name = 'upload.html'
     success_url = '/success/'
+    login_url = '/admin/login'
 
     def form_invalid(self, form):
         return super(AttendeesUploadFormView, self).form_valid(form)
