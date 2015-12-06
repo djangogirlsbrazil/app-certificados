47da393a50f3110e9e0de4666df1edeeba7c7f3a
Annanda Sousa <annanda.sousa@gmail.com>
2015-12-06 20:04:37 -0200

    Trocando o success_url

--- attendees_certification/views.py
+++ attendees_certification/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.views.generic.edit import FormView
+from django.views.generic import FormView
 
 from braces.views import LoginRequiredMixin
 
@@ -10,11 +10,15 @@ from .forms import AttendeesForm
 class AttendeesUploadFormView(LoginRequiredMixin, FormView):
     form_class = AttendeesForm
     template_name = 'upload.html'
-    success_url = '/success/'
+    success_url = '/admin/'
     login_url = '/admin/login'
 
     def form_invalid(self, form):
-        return super(AttendeesUploadFormView, self).form_valid(form)
+        # import ipdb; ipdb.set_trace()
+        return super(AttendeesUploadFormView, self).form_invalid(form)
 
     def form_valid(self, form):
+        # import ipdb; ipdb.set_trace()
+        csv_file = self.get_form()
         return super(AttendeesUploadFormView, self).form_valid(form)
+
