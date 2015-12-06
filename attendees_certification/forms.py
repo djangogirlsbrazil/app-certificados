385dd4697c816fc71e0b36539e2015140b593f8f
Annanda Sousa <annanda.sousa@gmail.com>
2015-12-06 21:47:54 -0200

    Adicionada a validação do tipo de arquivo.

--- attendees_certification/forms.py
+++ attendees_certification/forms.py
@@ -1,6 +1,12 @@
 # coding: utf-8
 from django import forms
+from django.core.exceptions import ValidationError
+
+
+def validate_file_extension(file):
+    if not file.name.endswith('.csv'):
+        raise ValidationError(u'O formato precisa ser .CSV')
 
 
 class AttendeesForm(forms.Form):
-    uploaded_file = forms.FileField(label=u'Arquivo CSV')
+    uploaded_file = forms.FileField(label=u'Arquivo CSV', validators=[validate_file_extension])
