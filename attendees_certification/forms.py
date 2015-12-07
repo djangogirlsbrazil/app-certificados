3e2435ae299af8248b9dfe5a417f8970bc40f1f6
Annanda Sousa <annanda.sousa@gmail.com>
2015-12-07 00:20:11 -0200

    Adicionado o form da view das participantes

--- attendees_certification/forms.py
+++ attendees_certification/forms.py
@@ -2,6 +2,8 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
+from .models import Attendees
+
 
 def validate_file_extension(file):
     if not file.name.endswith('.csv'):
@@ -10,3 +12,16 @@ def validate_file_extension(file):
 
 class AttendeesForm(forms.Form):
     uploaded_file = forms.FileField(label=u'Arquivo CSV', validators=[validate_file_extension])
+
+
+class AttendeesCertificationForm(forms.Form):
+    email = forms.EmailField(label=u'Email')
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        try:
+            Attendees.objects.get(email=data)
+        except Attendees.DoesNotExist:
+            raise ValidationError(u'Email não cadastrado')
+
+        return data
